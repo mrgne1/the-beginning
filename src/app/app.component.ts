7a8953fb168d63985bf8b9534407b952162422bc
mrgne1 <mrgne1@gmail.com>
2022-04-13 09:14:20 -0700

    Created the demo chart from angular-plotly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,27 @@
 import { Component } from '@angular/core';
 
+const lineTrace = {
+  type: 'scatter',
+  mode: 'lines',
+  x: [1, 2, 3],
+  y: [2, 6, 3],
+  line: {
+    color: 'red',
+  },
+};
+
+const barTrace = {
+  type: 'bar',
+  x: [1, 2, 3],
+  y: [2, 5, 3],
+};
+
+const layout = {
+  width: 320,
+  height: 240,
+  title: 'A Fancy Plot'
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +29,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'the-beginning';
+  graph: {data: Object[], layout: Object} = {
+    data: [lineTrace, barTrace],
+    layout: layout,
+  };
 }
